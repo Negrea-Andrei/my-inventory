0c5d81e49e29f9fab74fab2a740cc5c9e32e0cf2
Negrea-Andrei <negrea97@yahoo.com>
2024-02-11 22:25:26 +0200

    added a port

--- app.js
+++ app.js
@@ -34,6 +34,10 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/store", storeRouter);
 
+// Set the port
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
