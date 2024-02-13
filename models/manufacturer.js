cc80de2f288323bbba267d22097f89c50d375d61
Andrei Negrea <negrea97@yahoo.com>
2024-02-13 21:59:26 +0200

    Update manufacturer.js

--- models/manufacturer.js
+++ models/manufacturer.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const ManufacturerSchema = new Schema({
     name: { type: String, required: true, maxLength: 100 },
-    description: { type: String, required: true, maxLength: 100 },
+    description: { type: String, required: true, maxLength: 800 },
 })
 
 ManufacturerSchema.virtual("url").get(function () {
